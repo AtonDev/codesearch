9a27e1dc184343aef7abc35884a0da8cd4acfb8b
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-10-25 14:03:07 -0700

    delete old config file

--- config.js
+++
@@ -1,4 +0,0 @@
-var config = module.exports = {}
-
-config.ybKey = "dj0yJmk9TUpxQmhWSWFUeUpoJmQ9WVdrOWRtNW5SVTloTnpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1iYQ--"
-config.ybSecret = "f36f9ee4ebfb3efebc3d9b2c7cf2428ba5cb9da3"
