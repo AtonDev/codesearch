dd002827e71b831192ab090b28d0ea121b9d14eb
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-11-13 11:34:45 -0800

    add models for keyword and infocard for testing in app

--- app.js
+++ app.js
@@ -8,11 +8,24 @@ var timeout = require('connect-timeout')
 var routes = require('./routes')
 var router = express.Router()
 var server
+var Schema = require('jugglingdb').Schema
 var app = express()
 
 var HC = new hipchat('a054b26a420f7c8f23f321f8134a3b')
 
+var schema = new Schema('postgres', {
+  database: 'codesearch'
+  // username: 'postgres',
+  // host: 'localhost',
+  // port: 5432,
+  // password: s.password,
+  // database: s.database,
+  // ssl: true,
+  // debug: false
+});
+
 //Configuration
+app.set('schema', schema)
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
@@ -24,7 +37,7 @@ app.use(function(req, res, next) {
   next()
 })
 app.use(bodyparser.urlencoded())
-app.use(timeout('30s'))
+app.use(timeout('20s'))
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(router)
@@ -65,11 +78,43 @@ router.post('/feedback', function(req, res) {
 
 
 
-
-
+// BASIC ERROR HANDLING
+process.on('uncaughtException', function (err) {
+  console.error('uncaughtException: ', err.message)
+  console.error(err.stack)
+  //process.exit(1)
+})
   
 
+// TEST
+var Keyword = schema.define('keyword',{
+  keyword: { type: String , unique: true, index: true}
+})
 
+var Infocard = schema.define('infocard', {
+  syntax: { type: String },
+  example: { type: String },
+  descritpion: { type: String },
+  sourceURL: { type: String },
+  language: { type: String },
+  date_updated: { type: Date }
+})
+
+
+Keyword.hasAndBelongsToMany('infocards')
+
+schema.automigrate()
+schema.isActual(function(err, actual) {
+  if (!actual) {
+    schema.autoupdate()
+  }
+})
+
+Keyword.create({keyword: 'list'}, function(err, user) {
+  console.log('something happened')
+})
+
+// INITIALISATION
 
 
 server = http.createServer(app)
@@ -78,11 +123,7 @@ server.on('error', function (err) {
   console.error(err)
 })
 
-process.on('uncaughtException', function (err) {
-  console.error('uncaughtException: ', err.message)
-  console.error(err.stack)
-  //process.exit(1)
-})
+
 
 server.listen(app.get('port'), function() {
   console.log('app listening at port: ' + app.get('port'))
